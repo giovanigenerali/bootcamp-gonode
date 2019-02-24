d3eb2660c33480ab0310f432d085c4233ecbd481
Giovani Generali <giovani.generali@gmail.com>
2019-02-24 09:42:26 -0300

    session

--- modulo2/src/server.js
+++ modulo2/src/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const session = require('express-session')
+const FileStore = require('session-file-store')(session)
 const nunjucks = require('nunjucks')
 const path = require('path')
 
@@ -17,6 +19,15 @@ class App {
         extended: true
       })
     )
+    this.express.use(session({
+      name: 'root',
+      secret: 'MyAppSecret',
+      resave: true,
+      store: new FileStore({
+        path: path.resolve(__dirname, '..', 'tmp', 'sessions')
+      }),
+      saveUninitialized: true
+    }))
   }
 
   views () {
