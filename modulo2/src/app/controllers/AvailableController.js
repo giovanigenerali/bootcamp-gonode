b52120642f8175f1a9c417367d67fa20cb0492ff
Giovani Generali <giovani.generali@gmail.com>
2019-02-24 15:42:32 -0300

    fix schedule array

--- modulo2/src/app/controllers/AvailableController.js
+++ modulo2/src/app/controllers/AvailableController.js
@@ -23,17 +23,15 @@ class AvailableController {
     })
 
     const schedule = [
-      '8:00',
-      '9:00',
+      '08:00',
+      '09:00',
       '10:00',
       '11:00',
-      '12:00',
       '13:00',
       '14:00',
       '15:00',
       '16:00',
-      '17:00',
-      '18:00'
+      '17:00'
     ]
 
     const available = schedule.map(time => {
