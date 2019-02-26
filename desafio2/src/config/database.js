6d0b28b0d75bc31058c3d97cf3dba698000e2ee8
Giovani Generali <giovani.generali@gmail.com>
2019-02-26 11:23:36 -0300

    update database config

--- desafio2/src/config/database.js
+++ desafio2/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: '127.0.0.1',
-  username: 'docker',
-  password: 'docker',
-  database: 'gonodedesafio2',
+  host: process.env.PG_HOST || '127.0.0.1',
+  username: process.env.PG_USERNAME || 'docker',
+  password: process.env.PG_PASSWORD || 'docker',
+  database: process.env.PG_DATABASE || 'gonodedesafio2',
   operatorAliases: false,
   define: {
     timestamps: true,
